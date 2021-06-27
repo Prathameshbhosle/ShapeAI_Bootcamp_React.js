20920dadfccc2ada44feb0ccc5a5d19908d84d51
Prathameshbhosle <86553395+Prathameshbhosle@users.noreply.github.com>
2021-06-27 16:41:00 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript using React.js</h1>
+      <p>
+        A basic web development React JS Bootcamp ans we are also providing the
+        certificate for the student
+      </p>
+
+      <h1>Javascript using Node.js</h1>
+      <p>This bootcamp will start from 27 june </p>
+    </div>
+  );
+}
+
+export default Info;
